2833b08209890270e261482b4977021c85540fec
John Boyes <154404+johnboyes@users.noreply.github.com>
2021-01-09 13:08:25 +0000

    Add explicit error when required env vars not set

--- internal/jira/jira.go
+++ internal/jira/jira.go
@@ -1,9 +1,31 @@
 // Package jira publishes Gauge specifications to Jira
 package jira
 
+import (
+	"github.com/agilepathway/gauge-jira/internal/env"
+	"github.com/agilepathway/gauge-jira/util"
+	"github.com/andygrunwald/go-jira"
+)
+
 // PublishSpecs publishes the given Gauge specifications to Jira
 func PublishSpecs(specFilenames []string) {
 	issues := newIssues()
 	issues.addSpecs(specFilenames)
 	issues.publish()
+}
+
+func jiraClient() *jira.Client {
+	jiraBaseURL := env.GetRequired("JIRA_BASE_URL")
+	jiraUsername := env.GetRequired("JIRA_USERNAME")
+	jiraToken := env.GetRequired("JIRA_TOKEN")
+
+	transport := jira.BasicAuthTransport{
+		Username: jiraUsername,
+		Password: jiraToken,
+	}
+
+	jiraClient, err := jira.NewClient(transport.Client(), jiraBaseURL)
+	util.Fatal("Error while creating Jira Client", err)
+
+	return jiraClient
 }
